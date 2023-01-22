5d06367c4647a6298fb784f79becdcbce0bea3e7
Shmuel Israel <shmuelsrl@gmail.com>
2023-01-21 23:53:14 -0500

    footer undate #1

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -18,8 +18,17 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            BBTK Documentation 
           </Link>
+        </div>
+        <p></p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/blog">
+            blog
+          </Link>
+          
         </div>
       </div>
     </header>
